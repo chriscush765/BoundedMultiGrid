6532dba660377144f2fa4bb2afd77eb289d7e024
Justin <justinchao20@gmail.com>
2015-03-05 00:06:38 -0500

    lel bad code pls no look

--- BoundedMultiGrid/src/gridProj/BoundedMultiGrid.java
+++ BoundedMultiGrid/src/gridProj/BoundedMultiGrid.java
@@ -4,8 +4,19 @@ import info.gridworld.grid.*;
 
 public class BoundedMultiGrid extends BoundedGrid {
 
-	public BoundedMultiGrid(int rows, int cols) {
+	public BoundedMultiGrid(int rows, int cols) 
+	{
 		super(rows, cols);
+	}
+	
+	public void add(Object o, int row, int col)
+	{
+		Location loc = new Location(row, col);
+		if(get(loc) == null)
+			put(loc, new ObjectStack());
+		Object r = get(loc);
+		if(r instanceof ObjectStack)
+			((ObjectStack) r).add(o);
 	}
 	
 }
